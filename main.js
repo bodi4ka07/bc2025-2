51e1d9f3c317ac3030cecd47b577eb586160096a
Bohdan bc2025-2 <bodi4ka07@gmail.com>
2025-09-14 18:58:04 +0300

    Node.js program constructs URL for V2 (profit/loss)

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-// main.js placeholder
+// main.js
+// Варіант 2 - Доходи та витрати банків (серпень 2024, період = місяць)
+
+// 1) Константи для запиту
+const date = "20240801";      // YYYYMM для серпня 2024
+const time_period = "m";    // 'm' = місяць 
+
+// 2) Базовий шлях API
+const apiPath = "/NBUStatService/v1/statdirectory/banksincexp";
+const base = "https://bank.gov.ua/";
+
+// 3) Імпорт класу URL
+const { URL } = require('url');
+
+// 4) Створюємо об'єкт URL
+const myUrl = new URL(apiPath, base);
+
+// 5) Додаємо параметри через SearchParams
+myUrl.searchParams.set("date", date);
+myUrl.searchParams.set("period", time_period);
+myUrl.searchParams.set("json", "");   // параметр 'json' для отримання JSON
+
+// 6) Виводимо сформований URL у консоль
+console.log(myUrl.toString());
